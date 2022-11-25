71ef0fbe4dfce98111314e33d961bb615df7f994
timothymureithi <timothy.mureithi@student.moringaschool.com>
2022-11-25 14:48:41 +0300

    added clients migration

---
+++ db/migrate/20221125114753_create_clients.rb
@@ -0,0 +1,12 @@
+class CreateClients < ActiveRecord::Migration[7.0]
+  def change
+    create_table :clients do |t|
+      t.string :username
+      t.string :email
+      t.string :password
+      t.string :location
+
+      t.timestamps
+    end
+  end
+end
